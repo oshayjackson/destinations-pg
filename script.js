00f1996b98aee8ba243de46c08d71704d4431e5a
Oshay Jackson <oshay@OSHAY.local>
2023-12-14 13:18:05 -0800

    event handlers form submission

--- script.js
+++ script.js
@@ -3,4 +3,9 @@
 user_input_form.addEventListener("submit", (e) => {
   // event object
   e.preventDefault();
+  const destination_name = destination_name.value;
+  const location_name = location_name.value;
+  const photoUrl = photoUrl.value || PLACE_HOLDER_URL;
+  const desc = description.value || PLACE_HOLDER_DESCRIPTION;
+  console.table(console.log(destination_name, location_name, photoUrl, desc));
 });
